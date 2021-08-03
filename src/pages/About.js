7a496b14622d3aa72587d526748b9709d6462ea5
Jace <bellcastle88@gmail.com>
2021-08-04 01:33:07 +0900

    route

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, deserunt beatae. Consequuntur tempore iure aperiam soluta hic deleniti perspiciatis officia voluptas, atque accusamus facilis nesciunt reprehenderit non accusantium vero neque sint saepe fugiat dolore ducimus placeat quidem, temporibus repellat. Itaque?
+      </p>
+    </section>
   )
 }
 
